4bdd92d22a1ed23973dd40b80786a5beb969b6ec
chauthutran <cthutran@gmail.com>
2026-05-28 21:03:18 +0900

    feat: add search functionality to org unit hierarchy

--- src/top-bar/org-unit-select/org-unit-select.jsx
+++ src/top-bar/org-unit-select/org-unit-select.jsx
@@ -1,5 +1,5 @@
 import i18n from '@dhis2/d2-i18n'
-import { OrganisationUnitTree, Divider } from '@dhis2/ui'
+import { OrganisationUnitTree, Divider, InputField } from '@dhis2/ui'
 import React from 'react'
 import { useAppContext } from '../../app-context/index.js'
 import { useSelectionContext } from '../../selection-context/index.js'
@@ -7,6 +7,8 @@ import { ContextSelect } from '../context-select/index.js'
 import { ApprovalStatusIconsLegend } from './approval-status-icons-legend.jsx'
 import { ApprovalStatusLabel } from './approval-status-label.jsx'
 import classes from './org-unit-select.module.css'
+import { useOrgUnitSearch } from './useOrgUnitSearch.js'
+import { isNullOrEmpty } from '../../utils/string-utils.js'
 
 export const ORG_UNIT = 'ORG_UNIT'
 
@@ -21,6 +23,9 @@ const OrgUnitSelect = () => {
         setOpenedSelect,
         attributeOptionCombo,
     } = useSelectionContext()
+
+    const { searchText, orgUnits, setSearchText } = useOrgUnitSearch()
+
     const open = openedSelect === ORG_UNIT
     const value = orgUnit?.displayName
     const roots = organisationUnits.map(({ id }) => id)
@@ -43,6 +48,34 @@ const OrgUnitSelect = () => {
 
     const requiredValuesMessage = getRequiredValuesMessage()
 
+    const renderOrgUnitTree = () => {
+        const isSearching = !isNullOrEmpty(searchText)
+
+        const roots = isSearching
+            ? orgUnits.map(({ id }) => id)
+            : organisationUnits.map(({ id }) => id)
+
+        const treeKey = isSearching ? `${searchText}-${new Date().getTime()}` : 'initial';
+
+        return (
+            <OrganisationUnitTree
+                dataTest="org-unit-selector-tree"
+                key={treeKey}
+                roots={roots}
+                onChange={onChange}
+                selected={selectedOrgUnitPath}
+                singleSelection
+                {...(!isSearching && { initiallyExpanded: initiallySelected })}
+                renderNodeLabel={({ label, node }) => (
+                    <ApprovalStatusLabel
+                        label={label}
+                        orgUnitId={node.id}
+                    />
+                )}
+            />
+        );
+    }
+
     return (
         <ContextSelect
             dataTest="org-unit-context-select"
@@ -57,20 +90,23 @@ const OrgUnitSelect = () => {
             popoverMaxWidth={400}
         >
             <div className={classes.popoverContainer}>
-                <div className={classes.scrollbox}>
-                    <OrganisationUnitTree
-                        roots={roots}
-                        onChange={onChange}
-                        initiallyExpanded={initiallySelected}
-                        selected={selectedOrgUnitPath}
-                        singleSelection
-                        renderNodeLabel={({ label, node }) => (
-                            <ApprovalStatusLabel
-                                label={label}
-                                orgUnitId={node.id}
-                            />
-                        )}
+ 		<div className={classes.inputContainer}>
+                    <InputField
+                        dense
+                        name="context-selection-org-unit-search"
+                        placeholder={i18n.t('Search org units')}
+                        value={searchText}
+                        onChange={({ value: nextValue }) => setSearchText(nextValue)}
+                        className={classes.searchInput}
                     />
+                </div>
+
+                <div className={classes.dividerContainer}>
+                    <Divider dense />
+                </div>
+
+                <div className={classes.scrollbox}>
+                    {renderOrgUnitTree()}
                 </div>
                 <Divider margin="0" />
                 <ApprovalStatusIconsLegend />
